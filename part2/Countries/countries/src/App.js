be283c7c734145dd5e5b7efdf8359dcc66177457
Oswe-gif <jhoanoswaldo72@gmail.com>
2023-12-05 00:04:12 -0500

    Add exercises 2.18.-2.20

--- part2/Countries/countries/src/App.js
+++ part2/Countries/countries/src/App.js
@@ -2,20 +2,80 @@ import { useState } from 'react';
 import countriesService from './services/countriesService';
 
 function App() {
+  const api_key = process.env.REACT_APP_API_KEY;
   const [countries, setContries]=useState([]);
+  const [temperature, setTemperature]=useState({temp: '', wind:'',icon:''});
   const [filter, setFilter]=useState('');
+  const [pushedButtonToShowInformation, setPushedButtonToShowInformation]=useState(['',false]);
 
   const handleFilter =(event)=>{
     setFilter(event.target.value)
-    countriesService.getByName(event.target.value).then(countriesFilter => {
-        setContries(countriesFilter===undefined ? []:countriesFilter.map(a =>a.name.common))  
-    });
+    setPushedButtonToShowInformation(false);
+    getCountryInformation(event.target.value);
 
+  }
+  const getCountryInformation=(cityName)=>{
+    countriesService.getByName(cityName).then(countriesFilter => {
+      setContries(countriesFilter===undefined ? []:countriesFilter)  
+  });
+  }
+  const getWeather=(cityName, limit, WEATHER_API_KEY)=>{
+    
+    countriesService.getLatLonCityByName(cityName,limit,WEATHER_API_KEY )
+    .then(cityInformation =>cityInformation[0])
+    .then(city =>countriesService.getWeatherByName(city.lat, city.lon,WEATHER_API_KEY))
+    .then(weather => setTemperature({
+      temp: weather.main.temp,
+      wind: weather.wind.speed,
+      icon: weather.weather[0].icon  
+    }));
+  }
+
+
+  const CountryInformation=(name)=>{
+    const country = countries.find(p => p.name.common === name);
+    getWeather(country.capital, 1, api_key)
+    return (
+      <>
+        <h1>{country.name.common}</h1>
+        <div>Capital {country.capital}</div>
+        <div>Population {country.population}</div>
+        <h2>Languages</h2>
+        <ul>{Object.values(country.languages).map(languages => <li key={languages}>{languages}</li>)}</ul>
+        <img src={country.flags.png} />
+        <h2>Weather in {country.capital}</h2>
+        <div>Temperature {temperature.temp} Kelvin</div>
+        <img src={`https://openweathermap.org/img/wn/${temperature.icon}@2x.png`}/>
+        <div>Wind {temperature.wind} m/s</div>
+
+    </>
+    )
+  }
+
+
+  const Display =()=>{
+    
+    if(countries.length >=10){
+      return(
+        <div>To many matches, specify another filter</div>
+      )
+    }
+    if(countries.length ===1){
+      return CountryInformation(countries[0].name.common);
+    }
+    if(pushedButtonToShowInformation[1]){
+      return CountryInformation(pushedButtonToShowInformation[0]);
+    }
+    else{
+      return(
+        <ul>{countries.map(conuntries => <li key={conuntries.name.common}>{conuntries.name.common} <button value={conuntries.name.common} onClick={(event)=>setPushedButtonToShowInformation([event.target.value,true])}>Show</button></li>)}</ul>
+      )
+    }
   }
   return (
     <div>
       <div>find countries <input value={filter} onChange={handleFilter}/></div>
-      {countries.length >=10 ? 'To many matches, specify another filter': <ul>{countries.map(names => <li key={names}>{names}</li>)}</ul>}
+      {Display()}
       
     </div>
   );
--- part2/Countries/countries/src/services/countriesService.js
+++ part2/Countries/countries/src/services/countriesService.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const baseURL ='https://restcountries.com/v3.1/name';
-
 const getByName=name=>{
-    return axios.get(`${baseURL}/${name}`).then(response => response.data).catch(error => console.log(error))
+    return axios.get(`https://restcountries.com/v3.1/name/${name}`).then(response => response.data).catch(error => console.log(error))
 }
-export default {getByName}
+const getLatLonCityByName=(cityName, limit,WEATHER_API_KEY)=>{
+    return axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${WEATHER_API_KEY}`).then(response =>response.data).catch(error => console.log(error))
+}
+const getWeatherByName=(lat, lon,WEATHER_API_KEY)=>{
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`).then(response => response.data).catch(error => console.log(error));
+}
+export default {getByName, getLatLonCityByName,getWeatherByName}
